889854863fb616ba9c11595820c723dcc2d5ff01
carlance <carlance123@gmail.com>
2014-03-04 11:52:26 +0000

    Update script1.sh
    modified script

--- script1.sh
+++ script1.sh
@@ -1,54 +1,54 @@
 #!/bin/bash
 
 get_request='get_request.tmp'
+get_request_1='get_request_1.tmp'
 ip_addr_get='ip_addr_get.tmp'
 FILE='ipv6.tmp'
 
-#exec < $get_request
-#while read line
-#do
-#cut -f 1 -d ' ' | sort -t . -k 1,1n -k 2,2n -k 3,3n -k 4,4n  > $ip_addr_get
-#done 
+printf "Script running...\n"
+
+#printf "--------BASH SCRIPT RESULTS-------\n\n" >>top10.txt
 
 #List top 10 visitors to site by IP address
-grep "GET" $1 > $get_request   #Grabs lines with the word GET
-cut -f 1 -d ' ' $get_request |  uniq -c > $ip_addr_get  #Cut 1st field split by space and count number of occurence
-cat $ip_addr_get| sort -nr  > top10ip.txt # Sort top 10 ip addresses and save result to text file
-
+printf "TOP 10 IP VISITORS\n" >> top10.txt
+grep "GET" $1 > $get_request_1 #Grabs lines with the word GET
+cat $get_request_1 | sort > $get_request # Sorts the lines first to uniq them easily
+cut -f 1 -d ' ' $get_request | uniq -c 	> $ip_addr_get #Cut 1st field split by space and count number of occurence
+cat $ip_addr_get| sort -nr | head >> top10.txt # Sort top 10 ip addresses and save result to text file
+printf "\n" >> top10.txt
 
 #List top 10 pdf downloads
-grep ".pdf" $1> pdfraw1.tmp  #Grabs lines with the word .pdf
-cat pdfraw1.tmp | cut -f 2 -d '"' > pdfraw2.tmp #Cuts the second field split by " and saves to tmp file
+printf "TOP 10 PDF DOWNLOADS\n" >> top10.txt
+grep ".pdf" $1> pdfraw1.tmp #Grabs lines with the word .pdf
+cat pdfraw1.tmp | cut -f 2 -d '"' > pdfraw2.tmp # sort file
 cat pdfraw2.tmp | cut -f 2 -d ' ' > pdfraw3.tmp #Takes previous file and cuts 2nd field split by space
-cat pdfraw3.tmp | uniq  -c > pdfraw4.tmp #Removes duplicates and adds number of same occurences
-cat pdfraw4.tmp | sort -nr | head > top10pdf.txt #Sort the top 10 pdf downloads from highest to lowest and save result to text file
+cat pdfraw3.tmp | sort > pdfraw4.tmp #sorts file
+cat pdfraw4.tmp | uniq -c > pdfraw5.tmp # counts number of occurrences
+cat pdfraw5.tmp | sort -nr | head >> top10.txt #Sort the top 10 pdf downloads from highest to lowest and save result to text file
+printf "\n" >> top10.txt
 
 
 
-#List top 5 browsers 
+#List top 5 browsers
+printf "TOP 5 BROWSERS\n" >> top10.txt
 grep "GET" $1 > get_only.tmp #Grabs lines with the word GET
-cat get_only.tmp | cut -f 6 -d '"' > get_browsers.tmp #Cuts 6th field split by "
-cat get_browsers.tmp | uniq  -c > get_browsers_uniq.tmp #Removes duplicates and adds number of same occurences
-cat get_browsers_uniq.tmp | sort -nr | head -n 5 > top5browser.txt # Sort top 5 web browsers used and save result to text file
+cut -f 6 -d '"' get_only.tmp > get_browsers1.tmp #Cuts 6th field split by " 
+cat get_browsers1.tmp | sort | uniq -c  > get_browsers_uniq.tmp #Counts number of occurences and sorts
+#cat get_browsers_uniq.tmp | uniq -c > get_browsers2.tmp
+cat get_browsers_uniq.tmp | sort -nr | head -n 5 >> top10.txt # Sort top 5 web browsers used and save result to text file
+printf "\n" >> top10.txt
 
 
 
 #List ipv6 visitors
-grep "::" $1 > $FILE  #Grabs lines with the characters ":"
-cut -d ' ' -f 1 $FILE |sort > ipv6raw1.tmp #Cuts first field split by space 
-cat ipv6raw1.tmp | uniq  -c > ipv6raw2.tmp # Removes duplicates and adds number of same occurences
-cat ipv6raw2.tmp | sort -nr > topipv6.txt #Save result to text file
+printf "TOP IPV6 VISITORS\n" >> top10.txt
+grep "::" $1 > $FILE #Grabs lines with the characters ":"
+cut -d ' ' -f 1 $FILE |sort > ipv6raw1.tmp #Cuts first field split by space
+cat ipv6raw1.tmp | uniq -c > ipv6raw2.tmp # Count number of occurence
+cat ipv6raw2.tmp | sort -nr |head >> top10.txt #Save result to text file
  
 
-# for ip in `cat $FILE |cut -d ' ' -f 1 |sort |uniq`;
-# do { 
-#  	COUNT=`grep ^$ip $FILE |wc -l`;
-# 	echo "$COUNT:   $ip" >> topipv6.txt
-#}; 	done
- 
-
+#remove .tmp files
 rm *.tmp
 
-
- 
-
+printf "Finished running...\n"
